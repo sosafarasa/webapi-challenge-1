de9b9f57f8f3ae0ab95442f37c3d178b61134544
Agustina Sosa <agustinasf85@gmail.com>
2019-06-14 11:42:31 -0400

    Configured script and added nodemon.

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Hello there! Your server is running.</h1>`);
+})
+
+module.exports = server;
