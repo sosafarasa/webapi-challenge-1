db554a6e844474fcccf1d7121c5ea1c45a84fe2b
Agustina Sosa <agustinasf85@gmail.com>
2019-06-14 13:05:55 -0400

    .post endpoint created

--- data/actionsRouter.js
+++ data/actionsRouter.js
@@ -24,4 +24,14 @@ router.get('/:id', async (req, res) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    try{
+        const addAction = await Actions.insert(req.body);
+        res.status(200).json({ message: 'Action added!' })
+    } catch(err) {
+        res.status(500).json({ message: 'Action could not be added.' });
+    }
+})
+
+
 module.exports = router;
--- data/projectsRouter.js
+++ data/projectsRouter.js
@@ -7,9 +7,9 @@ router.get('/', async (req, res) => {
         const projects = await Projects.get();
         res.status(200).json(projects);
     } catch(err) {
-        res.status(500).json({ error: 'The project requested could not be retrieved.' })
+        res.status(500).json({ error: 'The project requested could not be retrieved.' });
     }
-})
+});
 
 router.get('/:id', async (req, res) => {
     try{
@@ -23,5 +23,17 @@ router.get('/:id', async (req, res) => {
         res.status(500).json({ error: 'The project requested could not be retrieved.' });
     }
 });
+
+router.post('/', async (req, res) => {
+    try {
+        const addProject = await Projects.insert(req.body);
+        res.status(200).json({ message: 'Project added!'});
+    } catch(err) {
+        res.status(500).json({ message: 'Project could not be added.' })
+    }
+});
+
+
+
 
 module.exports = router;
